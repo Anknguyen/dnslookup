dd9b94a397a8c676699637b57119a179f65dc28b
An <akimnguyen1999@gmail.com>
2022-01-29 17:20:43 -0500

    Initial commit

---
+++ src/main/java/com/met/dnslookup/DnslookupApplication.java
@@ -0,0 +1,13 @@
+package com.met.dnslookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnslookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnslookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/met/dnslookup/DnslookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.met.dnslookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnslookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
